59b880b56e4d5f5786d756e163678f3be0421167
GAYL Nabil <gaylnabil@gmail.com>
2021-09-14 15:28:09 +0100

    upload project

--- oh_hr_zk_attendance/zkaccess.py
+++
@@ -1,6 +0,0 @@
-from pyzkaccess import ZKAccess
-
-connstr = 'protocol=TCP,ipaddress=10.220.1.5,port=4370,timeout=4000,passwd='
-zk = ZKAccess(connstr=connstr)
-print(zk.connect)
-print('Device SN:', zk.parameters.serial_number, 'IP:', zk.parameters.ip_address)
